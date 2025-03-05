fc10c7e6403affa193056cdf3d8cd43263b8cb22
Bal67 <bal67@duke.edu>
2025-03-04 19:21:03 -0600

    Fix generator

--- generator.py
+++ generator.py
@@ -13,9 +13,9 @@ def generate_response(query):
 
         response = llm_pipeline(
             prompt,
-            max_length=200, 
+            max_length=250, 
             do_sample=False,
-            temperature=0.7,  
+            temperature=0.8,  
             top_p=0.9,  
             pad_token_id=50256,  
             eos_token_id=50256  
