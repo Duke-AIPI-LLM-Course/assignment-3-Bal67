b473b721285c03fe7e5c577e1a28fb3d0ab171e9
Bal67 <bal67@duke.edu>
2025-03-05 00:01:13 -0600

    fix

--- generator.py
+++ generator.py
@@ -20,7 +20,7 @@ def generate_response(query):
 
         response = llm_pipeline(
             prompt,
-            max_length=300,  
+            max_length=400,  
             do_sample=True,
             temperature=0.8,  
             top_p=0.9,
